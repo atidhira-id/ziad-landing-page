556ab4599fb37af722388604bb45d84be3919845
Galih Adhi Kusuma <noxturne.production@gmail.com>
2023-03-24 05:05:35 +0700

    fix(next): next js static build config, add prefix of . to assets

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  assetPrefix: '.',
+  images: {
+    unoptimized: true,
+  }
 }
 
 module.exports = nextConfig
