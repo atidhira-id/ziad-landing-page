a92b3cb3c56caf80b94a66783682a2deb836f4bc
Galih Adhi Kusuma <noxturne.production@gmail.com>
2023-03-24 05:06:57 +0700

    style(banner-product): change banner style layout using grid, and add animation

--- src/components/BannerProduct.jsx
+++ src/components/BannerProduct.jsx
@@ -1,30 +1,40 @@
 /* eslint-disable max-len */
 import React from 'react'
+import { motion } from 'framer-motion';
 
 function BannerProduct() {
   return (
-    <article className="linear-primary w-4/5 px-4 py-12 12 mx-auto mb-12 mt-8 text-center text-white rounded-lg shadow-lg">
-      <h3 className="font-poppins mb-8 font-bold text-xl text-center">
+    <motion.article
+      initial={{ opacity: 0, x: -50 }}
+      whileInView={{ opacity: 1, x: 0 }}
+      transition={{ ease: 'easeInOut' }}
+      className="linear-primary w-4/5 px-4 max-h-max min-h-[50vh] py-8 mx-auto mb-12 mt-8 grid grid-cols-2 grid-rows-1 text-center text-white rounded-lg shadow-lg"
+    >
+      <h3 className="mb-8 font-poppins font-bold text-xl text-center col-start-1 col-end-3 row-end-1">
         ZIAD Smart Education System menggunakan teknologi kekinian.
       </h3>
-      <p className="w-4/5 m-auto font-light">
-        Kami terus menerapkan teknologi terbaru untuk memastikan semua lembaga pendidikan dapat
-        memanfaatkannya. Teknologi ZIAD dikembangkan untuk kemudahan semua kegiatan agar lebih efektif
-        dan efisien.
-      </p>
-      <div className="flex justify-center items-center">
-        <p className="text-left">
+      <div className="col-start-1 col-end-1 row-start-1">
+        <p className="w-4/5 m-auto font-medium text-left">
+          Kami terus menerapkan teknologi terbaru untuk memastikan semua lembaga pendidikan dapat
+          memanfaatkannya. Teknologi ZIAD dikembangkan untuk kemudahan semua kegiatan agar lebih efektif
+          dan efisien.
+        </p>
+      </div>
+      <div className="col-start-2 col-end-2 row-start-1">
+        <p className="text-left font-light">
           1. Tagihan dan Pembayaran terintegrasi dengan BANK, Alfamart, Indomaret, Tokopedia dll.
           <br />
+          <br />
           2. Kartu siswa multifungsi menggunakan SmartCard.
-        </p>
-        <p className="text-left">
+          <br />
+          <br />
           3. Absensi guru terintegrasi dengan jadwal mengajar.
+          <br />
           <br />
           4. Aplikasi mobile untuk memudahkan akses pihak sekolah dan orang tua.
         </p>
       </div>
-    </article>
+    </motion.article>
   )
 }
 
