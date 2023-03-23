b0a90e7e77bff63d50fd8d80f253f8626ce10270
Galih Adhi Kusuma <noxturne.production@gmail.com>
2023-03-24 05:08:18 +0700

    feat(animation): add animation to card clients and card feature

--- src/components/CardClients.jsx
+++ src/components/CardClients.jsx
@@ -1,20 +1,26 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/self-closing-comp */
 /* eslint-disable jsx-a11y/img-redundant-alt */
-import React from 'react'
+import { motion } from "framer-motion";
 
 function CardClients({ children: text = 'text', imgUrl }) {
+  const randomNumber = Math.floor(Math.random() * 101) - 50;
   return (
-    <div className="min-w-[18rem] max-w-[20rem] flex items-center justify-center">
+    <motion.div
+      initial={{ opacity: 0, x: randomNumber }}
+      whileInView={{ opacity: 1, x: 0 }}
+      transition={{ ease: 'easeInOut', delay: 0.3 }}
+      className="min-w-[18rem] max-w-[20rem] flex items-center justify-center"
+    >
       <div className="w-full h-full px-12 py-4 flex items-center justify-center gap-4 bg-white shadow-lg rounded-md">
         {/* <img src={} alt="image" /> */}
         <div className="w-10 h-10 bg-blue-500">
         </div>
-        <h3 className="text-xl font-bold ">
+        <h3 className="text-xl font-bold w-fit">
           {text}
         </h3>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
--- src/components/CardFeatures.jsx
+++ src/components/CardFeatures.jsx
@@ -1,20 +1,27 @@
 /* eslint-disable react/self-closing-comp */
-import React from 'react';
+import { motion } from 'framer-motion'
 
 function CardFeatures({ children: desc, title = 'title' }) {
   return (
-    <div className="flex items-center justify-center bg-white">
-      <div className="w-72 h-80 max-h-max px-4 py-4 flex flex-col items-center justify-center shadow-lg rounded-lg">
+    <motion.div
+      initial={{opacity: 0, y: -50}}
+      whileInView={{opacity: 1, y: 0}}
+      transition={{ease: 'easeInOut', delay: 0.3}}
+      className="flex items-center justify-center bg-white"
+    >
+      <div className="w-72 h-80 max-h-max px-4 pb-4 pt-8 flex flex-col items-center justify-center shadow-lg rounded-lg">
+        {/* title */}
+        <h3 className="w-fit mb-3 font-poppins font-bold text-xl text-blue-600 text-center">{title}</h3>
         {/* image */}
         <div className="w-16 h-16 bg-blue-500 mb-8"></div>
-        {/* title */}
-        <h3 className="w-3/4 mb-3 font-poppins font-bold text-xl text-blue-600 text-center">{title}</h3>
         {/* desc */}
-        <p className="w-full text-center text-gray-500">
-          {desc}
-        </p>
+        <div className="grow">
+          <p className="w-full text-center text-gray-500">
+            {desc}
+          </p>
+        </div>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
