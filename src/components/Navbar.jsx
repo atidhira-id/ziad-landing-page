56bc6583fa782b34d50aa2e90519dfb342fa7db7
Galih Adhi Kusuma <noxturne.production@gmail.com>
2023-03-24 12:56:31 +0700

    fix(navbar): fix navbar mobile styling and issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,30 +14,35 @@ function Navbar() {
 
   return (
     <>
-      <nav className="hidden md:flex bg-brand md:w-full md:px-6 py-5 items-center jusitfy-center">
+      <nav className="sticky top-0 hidden md:flex linear-secondary md:w-full md:px-6 py-5 items-center jusitfy-center z-40">
         {/* logo */}
         <div className="md:block md:grow">
           <h1 className="font-open-sans font-bold text-xl text-white">Ziad</h1>
         </div>
         {/* navbar links */}
         <div className="hidden md:flex md:w-full flex-col md:flex-row items-center justify-center gap-4 font-regular text-sm text-white">
-          <a href="#">Home</a>
-          <a href="#">Features</a>
-          <a href="#">Product</a>
-          <a href="#">Contact</a>
+          <a href="#home">Home</a>
+          <a href="#features">Features</a>
+          <a href="#product">Product</a>
+          <a href="#contact">Contact</a>
           <div className="hidden md:block">
-            <Button variant="fill" color="white">
-              Try demo
-            </Button>
+            <a href="#demo" className="block">
+              <Button variant="fill" color="white">
+                Try demo
+              </Button>
+            </a>
           </div>
         </div>
       </nav>
       {/* hamburger icon */}
       <AnimatePresence>
-        <div className="w-full bg-brand">
+        <div className="md:hidden sticky w-full bg-brand flex justify-center items-center py-2 px-3">
           <button type="button" className="md:hidden" onClick={togglePhoneMenu}>
             <HamburgerIcon />
           </button>
+          <div className="md:block grow text-center">
+            <h1 className="font-open-sans font-bold text-xl text-white">Ziad</h1>
+          </div>
         </div>
         {isPhoneMenuOpen && (
           <motion.nav
@@ -45,22 +50,24 @@ function Navbar() {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             transition={{ ease: 'easeInOut' }}
-            className="fixed top-0 right-0 bottom-0 w-full h-screen md:hidden flex flex-col justify-start items-end bg-black bg-opacity-30 z-40"
+            className="fixed top-0 left-0 w-screen bottom-0 h-screen md:hidden flex flex-col justify-start items-end bg-black bg-opacity-30 z-40"
           >
             {/* phone menu */}
-            <div className="grow w-3/4 bg-brand flex flex-col md:flex-row items-end justify-center font-regular text-sm text-white">
-              <button type="button" className="mb-4" onClick={togglePhoneMenu}>
+            <div className="grow w-3/4 px-4 bg-brand flex flex-col md:flex-row items-end justify-center font-regular text-sm text-white">
+              <button type="button" className="mb-4 mr-4 mt-4" onClick={togglePhoneMenu}>
                 <CloseIcon />
               </button>
-              <div className="grow flex flex-col gap-4 items-end justify-center">
-                <a href="#">Home</a>
-                <a href="#">Features</a>
-                <a href="#">Product</a>
-                <a href="#">Contact</a>
+              <div className="grow flex flex-col gap-4 items-end justify-start mr-4 mt-5">
+                <a href="#home">Home</a>
+                <a href="#features">Features</a>
+                <a href="#product">Product</a>
+                <a href="#contact">Contact</a>
                 <div className="md:block">
-                  <Button variant="fill" color="white">
-                    Try demo
-                  </Button>
+                  <a href="#demo" className="block">
+                    <Button variant="fill" color="white">
+                      Try demo
+                    </Button>
+                  </a>
                 </div>
               </div>
             </div>
