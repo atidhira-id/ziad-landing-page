ae0177dc1f3ffee7c8bbea4bd91a9e889e6f0a23
Galih Adhi Kusuma <noxturne.production@gmail.com>
2023-03-24 07:36:49 +0700

    fix(donwload-image): fix download image size

--- src/components/DownloadLink.jsx
+++ src/components/DownloadLink.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function DownloadLink() {
   return (
     <div className="w-md max-w-md">
-      <img src="./download-google.jpg" alt="playstore-download-link" height="auto" width="100px" />
+      <img src="./download-google.jpg" alt="playstore-download-link" height="auto" width="150px" />
     </div>
   )
 }
