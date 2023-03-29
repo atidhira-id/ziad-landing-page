f5a91947963c2ae9d7e1d19211631a9f171ffe52
Galih Adhi Kusuma <noxturne.production@gmail.com>
2023-03-29 15:09:40 +0700

    feat(comp/wrapper): add new wrapper components
    added new slider wrapper components for cliend card for better layouting

---
+++ src/components/ClientsSlideWrapper.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import CardClients from './CardClients'
+
+function ClientsSlideWrapper({ clients }) {
+  console.log(clients);
+  return (
+    <section className="flex flex-col items-center justify-center h-full w-full gap-8">
+      <div className="flex flex-col md:flex-row items-center justify-center gap-8 w-full">
+        {clients && clients.map((client, i) => {
+          if (i <= 1) {
+            return (
+              <CardClients key={client.name}>{client.name}</CardClients>
+            )
+          }
+        })}
+      </div>
+      <div className="flex flex-col md:flex-row items-center justify-center gap-8 w-full">
+        {clients && clients.map((client, i) => {
+          if (i >= 2) {
+            return (
+              <CardClients key={client.name}>{client.name}</CardClients>
+            )
+          }
+        })}
+      </div>
+    </section>
+  )
+}
+
+export default ClientsSlideWrapper
