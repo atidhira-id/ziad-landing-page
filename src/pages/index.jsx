7b5c9ed9cf4497edf814a5ca9aaf572ac161886b
Galih Adhi Kusuma <noxturne.production@gmail.com>
2023-03-30 15:13:59 +0700

    fix(pages/index): fixed absolute path import error

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,14 @@
-import Footer from 'components/Footer';
-import Navbar from 'components/Navbar';
-import Jumbotron from 'components/sections/Jumbotron';
-import Clients from 'components/sections/Clients';
-import AboutUsQuotes from 'components/sections/AboutUsQuotes';
-import Features from 'components/sections/Features';
-import Products from 'components/sections/Products';
-import DownloadDemo from 'components/sections/DownloadDemo';
-import Pricing from 'components/sections/Pricing';
-import FloatingContactButton from 'components/FloatingContactButton';
+/* eslint-disable import/extensions */
+import Footer from '@/components/Footer';
+import Navbar from '@/components/Navbar';
+import Jumbotron from '@/components/sections/Jumbotron';
+import Clients from '@/components/sections/Clients';
+import AboutUsQuotes from '@/components/sections/AboutUsQuotes';
+import Features from '@/components/sections/Features';
+import Products from '@/components/sections/Products';
+import DownloadDemo from '@/components/sections/DownloadDemo';
+import Pricing from '@/components/sections/Pricing';
+import FloatingContactButton from '@/components/FloatingContactButton';
 
 export default function Home() {
   return (
