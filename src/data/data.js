cae9c36f69aa01d5f600d0bfd0ebfcd86979a2f9
Galih Adhi Kusuma <noxturne.production@gmail.com>
2023-04-03 02:53:28 +0700

    feat(data.js): create separate file to store data contents

---
+++ src/data/data.js
@@ -0,0 +1,81 @@
+export const clients = [
+  {
+    name: 'Mthq',
+    logo: './client-logo-one.png',
+  },
+  {
+    name: 'Al Muhajirin',
+    logo: './client-logo-one.png',
+  },
+  {
+    name: 'Ponpes Modern',
+    logo: './client-logo-one.png',
+  },
+  {
+    name: 'Tiara Aksara',
+    logo: './client-logo-one.png',
+  },
+  {
+    name: 'Daarel Qurro',
+    logo: './client-logo-one.png',
+  },
+  {
+    name: 'Daarul Hikmah',
+    logo: './client-logo-one.png',
+  },
+  {
+    name: 'Daarul Hikmah 1',
+    logo: './client-logo-one.png',
+  },
+  {
+    name: 'Daarul Hikmah 2',
+    logo: './client-logo-one.png',
+  },
+  {
+    name: 'Daarul Hikmah 4',
+    logo: './client-logo-one.png',
+  },
+];
+
+export const features = [
+  {
+    title: 'Otomasi pembayaran',
+    imageUrl: './payment-icon.png',
+    desc: 'memudahkan penerimaan pembayaran dan memudahkan wali santri membayar tagihan',
+  },
+  {
+    title: 'Otomasi Pembayaran',
+    imageUrl: './payment-icon.png',
+    desc: 'memudahkan penerimaan pembayaran dan memudahkan wali santri membayar tagihan',
+  },
+  {
+    title: 'Absensi Terintegrasi',
+    imageUrl: './file-icon.png',
+    desc: 'memudahkan memantau keaktifan guru dan santri',
+  },
+  {
+    title: 'Keuangan',
+    imageUrl: './money-icon.png',
+    desc: 'mengelola keuangan agar data rapi dan mengurangi kesalahan pencatatan',
+  },
+  {
+    title: 'Administrasi',
+    imageUrl: './file-icon.png',
+    desc: 'mengelola seluruh data agar mudah diakses dan dibagikan',
+  },
+  {
+    title: 'Administrasi',
+    imageUrl: './file-icon.png',
+    desc: 'mengelola seluruh data agar mudah diakses dan dibagikan',
+  },
+  {
+    title: 'Administrasi',
+    imageUrl: './file-icon.png',
+    desc: 'mengelola seluruh data agar mudah diakses dan dibagikan',
+  },
+  {
+    title: 'Administrasi',
+    imageUrl: './file-icon.png',
+    desc: 'mengelola seluruh data agar mudah diakses dan dibagikan',
+  },
+];
