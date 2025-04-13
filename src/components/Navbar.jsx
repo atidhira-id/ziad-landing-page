d0025ab2d3c2dc82dc8bb1fc237c5c9057fe1a04
Atidhira Habibillah <atidhiraid68@gmail.com>
2025-04-13 16:35:10 +0700

    fix: update navbar style and add social media icons into sidenav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,20 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable react/react-in-jsx-scope */
-import { useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
-import { CloseIcon, HamburgerIcon } from './Icons';
-import Button from './Button';
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { CloseIcon, HamburgerIcon, ArrowRightIcon } from "./Icons";
+import { IoLogoYoutube, IoLogoInstagram } from "react-icons/io";
+import Button from "./Button";
 
 function Navbar() {
-  const [isPhoneMenuOpen, setIsPhoneMenuOpen] = useState(false);
+  const [isPhoneMenuOpen, setIsPhoneMenuOpen] = useState(true);
+
+  const links = [
+    { link: "home", name: "Home" },
+    { link: "features", name: "Fitur Unggulan" },
+    { link: "product", name: "Produk Kami" },
+    { link: "contact", name: "Kontak" },
+  ];
 
   const togglePhoneMenu = () => {
     setIsPhoneMenuOpen(!isPhoneMenuOpen);
@@ -14,60 +22,114 @@ function Navbar() {
 
   return (
     <>
-      <nav className="hidden md:flex linear-secondary md:w-full md:px-6 py-5 items-center jusitfy-end z-40">
-        {/* logo */}
-        <div className="md:block md:grow">
-          <h1 className="font-open-sans font-bold text-xl text-white">ZIAD</h1>
-        </div>
-        {/* navbar links */}
-        <div className="hidden md:flex md:w-full flex-col md:flex-row items-center justify-end gap-4 font-regular text-sm text-white">
-          <a href="#home">Home</a>
-          <a href="#features">Fitur Unggulan</a>
-          <a href="#product">Produk Kami</a>
-          <a href="#contact">Kontak</a>
-          <div className="hidden md:block">
+      {/* Desktop Navigation */}
+      <div className="linear-secondary hidden w-full md:flex md:fixed z-40">
+        <nav className="w-full h-16 max-w-screen-xl mx-auto px-6 md:flex items-center justify-between">
+          {/* logo */}
+          <div className="block">
+            <h1 className="font-open-sans font-bold text-2xl text-white">
+              ZIAD
+            </h1>
+          </div>
+          {/* navbar links */}
+          <div className="grow flex flex-row items-center justify-center gap-8 lg:gap-10">
+            {links.map((link, key) => {
+              return (
+                <a
+                  href={`#${link.link}`}
+                  key={key}
+                  className="font-regular text-base lg:text-lg text-white tracking-wider opacity-80 hover:opacity-100"
+                >
+                  {link.name}
+                </a>
+              );
+            })}
+          </div>
+          <div className="block">
             <a href="#demo" className="block">
               <Button variant="fill" color="white">
                 Coba Aplikasi
               </Button>
             </a>
           </div>
+        </nav>
+      </div>
+      {/* Mobile Navigation */}
+      <nav className="md:hidden fixed top-0 w-full bg-brand flex justify-between items-center py-4 px-10 z-40">
+        <div className="md:block">
+          <h1 className="font-open-sans font-bold text-xl text-white">ZIAD</h1>
         </div>
+        <button
+          type="button"
+          className="md:hidden -scale-x-100"
+          onClick={togglePhoneMenu}
+        >
+          <HamburgerIcon />
+        </button>
       </nav>
-      {/* hamburger icon */}
+
+      {/* Side Navigation */}
       <AnimatePresence>
-        <div className="md:hidden fixed top-0 w-full bg-brand flex justify-center items-center py-2 px-3 z-40">
-          <button type="button" className="md:hidden" onClick={togglePhoneMenu}>
-            <HamburgerIcon />
-          </button>
-          <div className="md:block grow text-center">
-            <h1 className="font-open-sans font-bold text-xl text-white">ZIAD</h1>
-          </div>
-        </div>
         {isPhoneMenuOpen && (
           <motion.nav
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ ease: 'easeInOut' }}
-            className="fixed top-0 left-0 w-screen bottom-0 h-screen md:hidden flex flex-col justify-start items-end bg-black bg-opacity-30 z-40"
+            initial={{ scale: 0, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            exit={{ scale: 0, opacity: 0 }}
+            transition={{ duration: "0.233" }}
+            className="fixed top-0 left-0 w-screen bottom-0 h-screen md:hidden z-40 origin-top-right overflow-hidden"
           >
-            {/* phone menu */}
-            <div className="grow w-3/4 px-4 bg-brand flex flex-col md:flex-row items-end justify-center font-regular text-sm text-white">
-              <button type="button" className="mb-4 mr-4 mt-4" onClick={togglePhoneMenu}>
+            {/* Mobile menu */}
+            <div className="grow w-full h-screen px-10 pt-4 pb-10 bg-brand flex flex-col md:flex-row items-end justify-center font-regular text-sm text-white">
+              {/* Close button */}
+              <button type="button" onClick={togglePhoneMenu}>
                 <CloseIcon />
               </button>
-              <div className="grow flex flex-col gap-4 items-end justify-start mr-4 mt-5">
-                <a href="#home">Home</a>
-                <a href="#features">Features</a>
-                <a href="#product">Product</a>
-                <a href="#contact">Contact</a>
-                <div className="md:block">
-                  <a href="#demo" className="block">
+              {/* Main Links */}
+              <div className="grow w-full flex flex-col mt-8 gap-4">
+                {links.map((link, key) => {
+                  return (
+                    <a
+                      key={key}
+                      className="flex justify-between items-center py-3 text-2xl uppercase tracking-wide border-b-2"
+                      href={`#${link.link}`}
+                      onClick={togglePhoneMenu}
+                    >
+                      <p>{link.name}</p>
+                      <ArrowRightIcon />
+                    </a>
+                  );
+                })}
+              </div>
+              {/* Bottom links */}
+              <div className="flex flex-col w-full gap-12">
+                <div className="flex justify-between items-center gap-1">
+                  <a href="#demo" className="block" onClick={togglePhoneMenu}>
                     <Button variant="fill" color="white">
-                      Try demo
+                      <p className="text-xs uppercase font-semibold">
+                        Coba Aplikasi
+                      </p>
                     </Button>
                   </a>
+                  <p className="text-sm">Tap. Download. Enjoy!</p>
+                </div>
+                <div className="flex justify-between items-center w-full">
+                  <p className="text-lg tracking-wider font-bold">
+                    ZIAD SYSTEM
+                  </p>
+                  <div className="flex gap-4">
+                    <a
+                      href="https://www.youtube.com/@ptmitracerdasnusantara"
+                      target="_blank"
+                    >
+                      <IoLogoYoutube size={36} />
+                    </a>
+                    <a
+                      href="https://www.instagram.com/ziadsistem/"
+                      target="_blank"
+                    >
+                      <IoLogoInstagram size={36} />
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
