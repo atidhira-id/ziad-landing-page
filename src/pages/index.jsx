f393979c7f593da94adc4bc7a1fb15bf8248c562
Atidhira Habibillah <atidhiraid68@gmail.com>
2025-04-16 18:10:42 +0700

    feat: add marquee component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,20 +1,22 @@
 /* eslint-disable import/extensions */
-import Footer from '@/components/Footer';
-import Navbar from '@/components/Navbar';
-import Jumbotron from '@/components/sections/Jumbotron';
-import Clients from '@/components/sections/Clients';
-import AboutUsQuotes from '@/components/sections/AboutUsQuotes';
-import Features from '@/components/sections/Features';
-import Products from '@/components/sections/Products';
-import DownloadDemo from '@/components/sections/DownloadDemo';
-import Pricing from '@/components/sections/Pricing';
-import FloatingContactButton from '@/components/FloatingContactButton';
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import Jumbotron from "@/components/sections/Jumbotron";
+import Clients from "@/components/sections/Clients";
+import AboutUsQuotes from "@/components/sections/AboutUsQuotes";
+import Features from "@/components/sections/Features";
+import Products from "@/components/sections/Products";
+import DownloadDemo from "@/components/sections/DownloadDemo";
+import Pricing from "@/components/sections/Pricing";
+import FloatingContactButton from "@/components/FloatingContactButton";
+import ClientsMarquee from "@/components/ClientsMarquee";
 
 export default function Home() {
   return (
     <main id="luxy" className="overflow-x-hidden max-w-scren">
       <Navbar />
       <Jumbotron />
+      <ClientsMarquee />
       <Clients />
       <Features />
       <AboutUsQuotes />
