e12d18999123bf8048fd768619664f18dc8cc9c4
Atidhira Habibillah <atidhiraid68@gmail.com>
2025-04-16 18:06:37 +0700

    feat: update styling

--- src/components/CardClients.jsx
+++ src/components/CardClients.jsx
@@ -2,27 +2,15 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/self-closing-comp */
 /* eslint-disable jsx-a11y/img-redundant-alt */
-import { motion } from 'framer-motion';
 
-function CardClients({ children: text = 'text', imgUrl }) {
-  const randomNumber = Math.floor(Math.random() * 101) - 50;
+function CardClients({ children: text = "text", imgUrl }) {
   return (
-    <motion.div
-      initial={{ opacity: 0, x: randomNumber }}
-      whileInView={{ opacity: 1, x: 0 }}
-      transition={{ ease: 'easeInOut', delay: 0.3 }}
-      className="min-w-[18rem] max-w-[20rem] flex items-center justify-center"
-    >
-      <div className="w-full h-full px-12 py-4 flex items-center justify-center gap-4 bg-white shadow-lg rounded-md">
-        {imgUrl && (
-          <img src={imgUrl} alt="image" width="80px" height="auto" />
-        )}
-        {/* <div className="w-10 h-10 bg-blue-500"> */}
-        <h3 className="grow text-xl font-bold w-full text-green-700">
-          {text}
-        </h3>
+    <div className="w-72 h-32 flex items-center justify-center">
+      <div className="bg-white w-full h-full px-10 py-4 flex items-center justify-center gap-4 shadow-lg rounded-md">
+        {imgUrl && <img src={imgUrl} alt="image" className="w-20" />}
+        <h3 className="grow text-lg font-bold text-green-700">{text}</h3>
       </div>
-    </motion.div>
+    </div>
   );
 }
 
