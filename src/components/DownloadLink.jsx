2e92c8c1950633702cc8f76bc6ebe8502f419c26
Atidhira Habibillah <atidhiraid68@gmail.com>
2025-04-21 03:10:14 +0700

    refactor: tidy up code

--- src/components/DownloadLink.jsx
+++ src/components/DownloadLink.jsx
@@ -1,13 +1,26 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 /* eslint-disable @next/next/no-img-element */
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 
 function DownloadLink({ platforms }) {
   return (
     <div className="w-md max-w-md">
-      <Link href="https://play.google.com/store/apps/details?id=com.ziad.pesantren">
-        <img src={platforms === 'playstore' ? './download-apple.jpg' : './download-google.jpg'} jpg alt="download-ziad-link-image-button" height="auto" width="150px" />
+      <Link
+        href="https://play.google.com/store/apps/details?id=com.ziad.pesantren"
+        target="_blank"
+      >
+        <img
+          src={
+            platforms === "playstore"
+              ? "./download-apple.jpg"
+              : "./download-google.jpg"
+          }
+          jpg
+          alt="download-ziad-link-image-button"
+          height="auto"
+          width="150px"
+        />
       </Link>
     </div>
   );
