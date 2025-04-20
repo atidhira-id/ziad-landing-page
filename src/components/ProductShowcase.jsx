97d70147a5d02920290b2e5e340c3091ee934905
Atidhira Habibillah <atidhiraid68@gmail.com>
2025-04-21 03:06:37 +0700

    refactor: tidy up code

--- src/components/ProductShowcase.jsx
+++ src/components/ProductShowcase.jsx
@@ -2,87 +2,50 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/img-redundant-alt */
-import React from 'react';
-import { motion } from 'framer-motion';
-import { Title } from './Typography';
+import React from "react";
+import { Title } from "./Typography";
 
-function ProductShowcase({
-  children: desc, title, imgUrl, align, imgSize,
-}) {
-  if (align === 'left') {
+function ProductShowcase({ children: desc, title, imgUrl, align, imgSize }) {
+  if (align === "left") {
     return (
-      // container
-      <motion.section
-        initial={{ opacity: 0, x: 100 }}
-        whileInView={{ opacity: 1, x: 0 }}
-        transition={{ delay: 0.4, ease: 'easeInOut' }}
-        viewport={{ margin: '100px' }}
-        className="w-full md:w-4/5 h-max md:min-h-[20vh] px-4 md:px-12 py-6 flex flex-col md:flex-row  jusftify-center items-center gap-8"
-      >
+      <section className="w-5/6 p-4 flex flex-col md:flex-row justify-between items-center gap-4 border border-gray-200 rounded-3xl">
         {/* product image */}
         <div>
-          <img
-            src={imgUrl || './phone-image.png'}
-            alt="phone-image-apps"
-            height="auto"
-            className={`w-[200px] md:w-[${imgSize || 400}px]`}
-          />
+          <img src={imgUrl} alt="product-image" className="w-56 md:w-72" />
         </div>
         {/* product desc */}
-        <article className="w-full md:w-3/4 md:mr-12">
+        <article className="w-full md:w-3/5">
           {/* title */}
           <div className="mb-4">
-            <Title
-              size="text-lg md:text-2xl"
-              align="text-center md:text-left"
-            >
+            <Title size="text-lg md:text-2xl" align="text-center md:text-left">
               {title}
             </Title>
           </div>
           {/* short desc */}
-          <p className="text-center md:text-left text-gray-500">
-            {desc}
-          </p>
+          <p className="text-center md:text-left text-gray-600">{desc}</p>
         </article>
-      </motion.section>
+      </section>
     );
   }
 
   return (
-    // container
-    <motion.section
-      initial={{ opacity: 0, x: -100 }}
-      whileInView={{ opacity: 1, x: 0 }}
-      transition={{ delay: 0.4 }}
-      viewport={{ margin: '100px' }}
-      className="w-full md:w-4/5 md:min-h-[25vh] h-max px-4 md:px-12 md:my-12 py-6 flex flex-col-reverse md:flex-row justify-center items-center gap-8 "
-    >
+    <section className="w-5/6 px-8 py-16 flex flex-col md:flex-row justify-between items-center gap-4 border border-gray-200 rounded-3xl">
       {/* product desc */}
-      <article className="w-full md:w-3/4 md:mr-12">
+      <article className="w-full md:w-1/2">
         {/* title */}
         <div className="mb-4">
-          <Title
-            size="text-lg md:text-2xl"
-            align="text-center md:text-left"
-          >
+          <Title size="text-lg md:text-2xl" align="text-center md:text-left">
             {title}
           </Title>
         </div>
         {/* short desc */}
-        <p className="text-center md:text-left text-gray-500">
-          {desc}
-        </p>
+        <p className="text-center md:text-left text-gray-600">{desc}</p>
       </article>
       {/* product image */}
       <div>
-        <img
-          src={imgUrl || './phone-image.png'}
-          alt="phone-image-apps"
-          height="auto"
-          className="w-[200px] md:w-[400px]"
-        />
+        <img src={imgUrl} alt="product-image" className="w-56 md:w-72" />
       </div>
-    </motion.section>
+    </section>
   );
 }
 
