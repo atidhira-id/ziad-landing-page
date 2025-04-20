57a3ada27c855070b8eb9bdee879482f87955585
Atidhira Habibillah <atidhiraid68@gmail.com>
2025-04-21 03:12:06 +0700

    feat: update styling

--- src/components/CardClients.jsx
+++ src/components/CardClients.jsx
@@ -5,10 +5,10 @@
 
 function CardClients({ children: text = "text", imgUrl }) {
   return (
-    <div className="w-72 h-32 flex items-center justify-center">
-      <div className="bg-white w-full h-full px-10 py-4 flex items-center justify-center gap-4 shadow-lg rounded-md">
-        {imgUrl && <img src={imgUrl} alt="image" className="w-20" />}
-        <h3 className="grow text-lg font-bold text-green-700">{text}</h3>
+    <div className="w-72 h-24 sm:h-30 flex items-center justify-center z-0">
+      <div className="bg-white w-full h-full px-10 py-4 flex items-center justify-center gap-4 border border-gray-300 rounded-md">
+        {imgUrl && <img src={imgUrl} alt="image" className="w-16 h-16" />}
+        <h3 className="grow text-base font-bold  text-green-700">{text}</h3>
       </div>
     </div>
   );
