0cbd3358d02d9857f9a1b0fb99692dfa681ba022
Atidhira Habibillah <atidhiraid68@gmail.com>
2025-04-21 03:15:07 +0700

    feat: update styling

--- src/components/sections/DownloadDemo.jsx
+++ src/components/sections/DownloadDemo.jsx
@@ -1,50 +1,43 @@
 /* eslint-disable max-len */
 /* eslint-disable jsx-a11y/img-redundant-alt */
 /* eslint-disable @next/next/no-img-element */
-import React from 'react';
-import Link from 'next/link';
-import DownloadLink from '../DownloadLink';
-import { SectionTitle } from '../Typography';
-import Button from '../Button';
+import React from "react";
+import DownloadLink from "../DownloadLink";
+import { SectionSubTitle, SectionTitle } from "../Typography";
 
 function DownloadDemo() {
   return (
-    // container
     <>
-      <section className="w-full h-max px-4 md:px-12 py-12 flex flex-col justify-center items-center">
-        <SectionTitle>Mau Mencoba Sistem Kami?</SectionTitle>
-        <div className="flex gap-4">
-          <Link href="http://wa.me/6287776669010">
-            <Button variant="fill" color="blue">Request Akun demo</Button>
-          </Link>
-          <Link href="https://demo2.ziadsistem.com/dashboard">
-            <Button variant="fill" color="blue">Coba Aplikasi Dashboard</Button>
-          </Link>
-        </div>
-      </section>
-      <section className="w-[90vw] h-max px-4 md:px-12 py-12 flex flex-col md:flex-row justify-center items-center " id="demo">
-        {/* product desc */}
-        <article className="w-full md:w-3/5 pl-12 flex flex-col align-start justify-center gap-4">
-          {/* title */}
-          <h3 className="w-full mb-4 font-bold text-5xl text-center md:text-left">
+      <section
+        className="w-5/6 max-w-7xl mx-auto pt-20 flex flex-col md:flex-row justify-center items-start"
+        id="demo"
+      >
+        <article className="w-full lg:w-3/5 flex flex-col align-center justify-center gap-4">
+          <SectionTitle className="lg:text-left">
             Coba
             <span className="text-blue-600"> Aplikasi</span>
             <br />
             Wali Murid
-          </h3>
-          {/* short desc */}
-          <p className="w-full md:w-3/4 text-center md:text-left text-gray-500">
-            Anda bisa langsung mencoba menggunakan Aplikasi Demo kami yang tersedia di PlayStore! Jika Anda tertarik dengan menggunakan Ziad System, Silahkan hubungi kami.
-          </p>
-          <div className="w-full flex items-center justify-center md:justify-start gap-8 my-2">
+          </SectionTitle>
+          <SectionSubTitle className="lg:w-3/4 lg:text-left lg:m-0">
+            Anda bisa langsung mencoba menggunakan Aplikasi Demo kami yang
+            tersedia di PlayStore! Jika Anda tertarik dengan menggunakan Ziad
+            System, Silahkan hubungi kami.
+          </SectionSubTitle>
+          <div className="w-full flex items-center justify-center lg:justify-start gap-8 my-4">
             {/* download link */}
             <DownloadLink platforms="appstore" />
             <DownloadLink platforms="playstore" />
           </div>
         </article>
         {/* product image on desktop */}
-        <div className="hidden md:block">
-          <img src="./demo-app-phone-mockup.png" alt="phone-image-apps" height="auto" className="w-[200px] md:w-[400px]" />
+        <div className="hidden lg:block">
+          <img
+            src="./demo-app-phone-mockup.png"
+            alt="phone-image-apps"
+            height="auto"
+            className="w-[200px] md:w-[300px]"
+          />
         </div>
       </section>
     </>
