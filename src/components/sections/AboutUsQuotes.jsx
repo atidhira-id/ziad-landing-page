2300a2e460102899d6c6b3a9f688cd47a6a3ed02
Atidhira Habibillah <atidhiraid68@gmail.com>
2025-04-21 03:17:20 +0700

    feat: update styling

--- src/components/sections/AboutUsQuotes.jsx
+++ src/components/sections/AboutUsQuotes.jsx
@@ -1,32 +1,42 @@
 /* eslint-disable max-len */
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 
 function AboutUsQuotes() {
   return (
-    <section className="w-full h-[60vh] md:h-[100vh] flex flex-col justify-center items-center px-2 md:px-4 md:mb-20">
-      {/* title */}
-      <div className="mb-8">
-        <a href="http://www.freepik.com" className="hidden">Designed by vectorjuice / Freepik</a>
-        <Image src="./islamic-school-vector.jpg" height={800} width={800} alt="islamic-school-pesantren-pesantren-modern" />
+    <section className="bg-gray-50 w-5/6 max-w-7xl mx-auto mt-20 flex flex-col lg:flex-row justify-center items-center gap-4 py-8 px-2 rounded-3xl">
+      {/* image */}
+      <div className="lg:basis-1/2">
+        <a href="http://www.freepik.com" className="hidden">
+          Designed by vectorjuice / Freepik
+        </a>
+        <Image
+          src="./islamic-school-vector.jpg"
+          height={600}
+          width={600}
+          alt="islamic-school-pesantren-pesantren-modern"
+          className="mix-blend-multiply"
+        />
       </div>
-      <div className="w-full md:w-3/4 text-center mb-3">
-        <h3 className="font-poppins font-bold text-xl md:text-2xl">
-          Membantu pesantren dan sekolah islam
-          <span className="text-blue-600">
-            {' '}
-            Memanfaatkan Teknologi
-          </span>
-        </h3>
+      <div className="lg:basis-1/2">
+        {/* title */}
+        <div className="w-full mb-3 lg:mb-8">
+          <h3 className="text-center lg:text-left font-bold text-xl md:text-3xl px-4">
+            Membantu Pesantren dan Sekolah Islam
+            <span className="text-blue-600"> Memanfaatkan Teknologi</span>
+          </h3>
+        </div>
+        {/* paragraph */}
+        <article className="w-full text-sm md:text-base text-center lg:text-left px-4 text-gray-600">
+          <p>
+            Kami mengembangkan ZIAD agar teknologi terkini dapat dimanfaatkan
+            untuk membantu lembaga pendidikan dalam mempermudah proses uang
+            pembayaran uang sekolah, absensi guru akurat sesuai jadwal,
+            mengelola keuangan sekolah, mengelola SDM, memberi pelayanan terbaik
+            untuk wali murid, dll.
+          </p>
+        </article>
       </div>
-      {/* paragraph */}
-      <article className="w-full md:w-3/4 px-2 text-sm md:text-md text-center text-gray-500">
-        <p>
-          Kami mengembangkan ZIAD agar teknologi terkini dapat dimanfaatkan untuk membantu lembaga pendidikan dalam
-          mempermudah proses uang pembayaran uang sekolah, absensi guru akurat sesuai jadwal, mengelola keuangan
-          sekolah, mengelola SDM, memberi pelayanan terbaik untuk wali murid dll
-        </p>
-      </article>
     </section>
   );
 }
