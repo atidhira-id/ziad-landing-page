580437e5709cfa7274c7705e8d62373c60d3c954
Atidhira Habibillah <atidhiraid68@gmail.com>
2025-04-21 03:09:45 +0700

    feat: update styling and refactor code

--- src/components/FeatureSlideSwapper.jsx
+++ src/components/FeatureSlideSwapper.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import CardFeatures from './CardFeatures';
+import React from "react";
+import CardFeatures from "./CardFeatures";
 
 function FeatureSlideWrapper({ features }) {
   return (
-    <div className="w-full h-max flex flex-col md:flex-row items-center justify-center gap-8 mb-20">
-      {features && features.map((feature) => (
-        <CardFeatures key={feature.title} title={feature.title} imageUrl={feature.imageUrl}>
-          {feature.desc}
-        </CardFeatures>
-      ))}
+    <div className="w-full flex flex-col md:flex-row items-center justify-center gap-4 mb-20">
+      {features &&
+        features.map((feature) => (
+          <CardFeatures
+            key={feature.title}
+            title={feature.title}
+            imageUrl={feature.imageUrl}
+          >
+            {feature.desc}
+          </CardFeatures>
+        ))}
     </div>
   );
 }
