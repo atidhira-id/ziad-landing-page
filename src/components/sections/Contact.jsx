f0d3bae430c0cba2dfa8ea2a2aa41153f219560b
Atidhira Habibillah <atidhiraid68@gmail.com>
2025-05-01 15:49:22 +0700

    feat: add placeholder text for wa message

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -3,12 +3,15 @@ import React from "react";
 import { SectionSubTitle, SectionTitle } from "../Typography";
 import { BsWhatsapp } from "react-icons/bs";
 import { MdDashboard } from "react-icons/md";
+import { whatsappTemplateText } from "@/data/data";
 
 function Contact() {
+  const encodedTeks = encodeURIComponent(whatsappTemplateText);
+
   return (
     <>
       <section
-        className="bg-[#101014] w-5/6 max-w-6xl mx-auto py-20 md:py-30 xl:py-40 px-6 flex flex-col justify-center items-center rounded-[2rem] text-white"
+        className="bg-blue-700 w-5/6 max-w-6xl mx-auto py-20 md:py-30 xl:py-40 px-6 flex flex-col justify-center items-center rounded-[2rem] text-white"
         id="contact"
       >
         <SectionTitle className="mb-8 md:mb-12 max-w-3xl mx-auto text-white !leading-snug">
@@ -20,9 +23,10 @@ function Contact() {
         </SectionSubTitle>
         <div className="w-full flex flex-col sm:flex-row justify-center items-center gap-4 mt-12">
           <a
-            href="http://wa.me/6287776669010"
+            href={`https://api.whatsapp.com/send?phone=6287776669010&text=${encodedTeks}`}
             target="_blank"
-            className="w-full max-w-xs p-4 text-sm border border-white flex justify-center items-center gap-2"
+            rel="noopener noreferrer"
+            className="w-full max-w-xs p-4 text-sm border-white border-2 flex justify-center items-center gap-2 rounded-xl hover:bg-blue-800"
           >
             <BsWhatsapp size={24} />
             Chat via WhatsApp
@@ -30,7 +34,7 @@ function Contact() {
           <a
             href="https://demo2.ziadsistem.com/dashboard"
             target="_blank"
-            className="w-full max-w-xs p-4 text-sm border border-white flex justify-center items-center gap-2"
+            className="w-full max-w-xs p-4 text-sm border-white border-2 flex justify-center items-center gap-2 rounded-xl hover:bg-blue-800"
           >
             <MdDashboard size={24} />
             Coba Dashboard ZIAD
