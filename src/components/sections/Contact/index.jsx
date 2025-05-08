a7a5ee06dedb219ddac83ff50374f12e85c93420
Atidhira Habibillah <atidhiraid68@gmail.com>
2025-05-09 01:10:22 +0700

    feat: update button and styles

--- src/components/sections/Contact/index.jsx
+++ src/components/sections/Contact/index.jsx
@@ -1,46 +1,50 @@
-/* eslint-disable max-len */
 import React from "react";
-import { SectionSubTitle, SectionTitle } from "../Typography";
+import { SectionSubTitle, SectionTitle } from "@/components/Typography";
 import { BsWhatsapp } from "react-icons/bs";
 import { MdDashboard } from "react-icons/md";
 import { whatsappTemplateText } from "@/data/data";
+import { SectionContainer } from "@/components/Containers";
+import { Button } from "@/components/Button";
 
 function Contact() {
   const encodedTeks = encodeURIComponent(whatsappTemplateText);
 
   return (
     <>
-      <section
-        className="bg-blue-700 w-5/6 max-w-6xl mx-auto py-20 md:py-30 xl:py-40 px-6 flex flex-col justify-center items-center rounded-[2rem] text-white"
-        id="contact"
-      >
-        <SectionTitle className="mb-8 md:mb-12 max-w-3xl mx-auto text-white !leading-snug">
-          Tertarik untuk menggunakan sistem kami?
-        </SectionTitle>
-        <SectionSubTitle className="mb-8 md:!text-xl !max-w-3xl text-white">
-          Diskusikan kebutuhan lembaga Anda dengan tim kami — gratis tanpa
-          biaya!
-        </SectionSubTitle>
-        <div className="w-full flex flex-col sm:flex-row justify-center items-center gap-4 mt-12">
-          <a
-            href={`https://api.whatsapp.com/send?phone=6287776669010&text=${encodedTeks}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="w-full max-w-xs p-4 text-sm border-white border-2 flex justify-center items-center gap-2 rounded-xl hover:bg-blue-800"
-          >
-            <BsWhatsapp size={24} />
-            Chat via WhatsApp
-          </a>
-          <a
-            href="https://demo2.ziadsistem.com/dashboard"
-            target="_blank"
-            className="w-full max-w-xs p-4 text-sm border-white border-2 flex justify-center items-center gap-2 rounded-xl hover:bg-blue-800"
-          >
-            <MdDashboard size={24} />
-            Coba Dashboard ZIAD
-          </a>
+      <SectionContainer className="pt-32" id="contact">
+        <div className="max-w-4xl mx-auto bg-blue-700 text-white flex flex-col justify-center items-center rounded-xl py-10 md:py-20">
+          <SectionTitle className="mb-8 md:mb-12 max-w-3xl mx-auto text-white !leading-snug">
+            Tertarik untuk menggunakan sistem kami?
+          </SectionTitle>
+          <SectionSubTitle className="mb-8 md:!text-xl !max-w-3xl text-white text-center">
+            Diskusikan kebutuhan lembaga Anda dengan tim kami — gratis tanpa
+            biaya!
+          </SectionSubTitle>
+          <div className="w-full flex flex-col sm:flex-row justify-center items-center gap-4 mt-12 z-0 ">
+            <a
+              href={`https://api.whatsapp.com/send?phone=6287776669010&text=${encodedTeks}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-5/6 max-w-xs"
+            >
+              <Button className="gap-2 w-full py-3">
+                <BsWhatsapp size={24} />
+                Chat via WhatsApp
+              </Button>
+            </a>
+            <a
+              href="https://demo2.ziadsistem.com/dashboard"
+              target="_blank"
+              className="w-5/6 max-w-xs"
+            >
+              <Button className="gap-2 w-full py-3">
+                <MdDashboard size={24} />
+                Coba Dashboard ZIAD
+              </Button>
+            </a>
+          </div>
         </div>
-      </section>
+      </SectionContainer>
     </>
   );
 }
