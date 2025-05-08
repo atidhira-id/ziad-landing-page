6c5b67e71bbc3c3444526f7457461bc4c9f783db
Atidhira Habibillah <atidhiraid68@gmail.com>
2025-05-09 01:08:19 +0700

    feat: update button

--- src/components/sections/About/index.jsx
+++ src/components/sections/About/index.jsx
@@ -1,43 +1,35 @@
-/* eslint-disable max-len */
+import { SectionContainer } from "@/components/Containers";
+import { SectionSubTitle, SectionTitle } from "@/components/Typography";
 import React from "react";
 
-function AboutUsQuotes() {
+function About() {
   return (
-    <section className="bg-gray-50 w-5/6 max-w-7xl mx-auto mt-20 flex flex-col lg:flex-row justify-center items-center gap-4 py-8 px-2 rounded-3xl">
-      {/* image */}
+    <SectionContainer className="bg-gray-50 mt-20 flex flex-col lg:flex-row justify-center items-center gap-4">
       <div className="lg:basis-1/2">
         <a href="http://www.freepik.com" className="hidden">
           Designed by vectorjuice / Freepik
         </a>
         <img
           src="./islamic-school-vector.jpg"
-          height={600}
-          width={600}
           alt="islamic-school-pesantren-pesantren-modern"
           className="mix-blend-multiply"
         />
       </div>
       <div className="lg:basis-1/2">
-        {/* title */}
-        <div className="w-full mb-3 lg:mb-8">
-          <h3 className="text-center lg:text-left font-bold text-xl md:text-3xl px-4">
-            Membantu Pesantren dan Sekolah Islam
-            <span className="text-blue-600"> Memanfaatkan Teknologi</span>
-          </h3>
-        </div>
-        {/* paragraph */}
-        <article className="w-full text-sm md:text-base text-center lg:text-left px-4 text-gray-600">
-          <p>
-            Kami mengembangkan ZIAD agar teknologi terkini dapat dimanfaatkan
-            untuk membantu lembaga pendidikan dalam mempermudah proses uang
-            pembayaran uang sekolah, absensi guru akurat sesuai jadwal,
-            mengelola keuangan sekolah, mengelola SDM, memberi pelayanan terbaik
-            untuk wali murid, dll.
-          </p>
-        </article>
+        <SectionTitle className="text-xl mb-8 px-8 lg:text-left">
+          Membantu Pesantren dan Sekolah Islam
+          <span className="text-blue-700"> Memanfaatkan Teknologi</span>
+        </SectionTitle>
+        <SectionSubTitle className="text-center px-3 lg:text-left lg:px-8">
+          Kami mengembangkan ZIAD agar teknologi terkini dapat dimanfaatkan
+          untuk membantu lembaga pendidikan dalam mempermudah proses uang
+          pembayaran uang sekolah, absensi guru akurat sesuai jadwal, mengelola
+          keuangan sekolah, mengelola SDM, memberi pelayanan terbaik untuk wali
+          murid, dll.
+        </SectionSubTitle>
       </div>
-    </section>
+    </SectionContainer>
   );
 }
 
-export default AboutUsQuotes;
+export default About;
