ec63a8bbda02bf52907f9fbcaff5b279c4455f0f
Atidhira Habibillah <atidhiraid68@gmail.com>
2025-05-09 01:12:22 +0700

    feat: update styles

--- src/components/sections/Download/index.jsx
+++ src/components/sections/Download/index.jsx
@@ -1,46 +1,39 @@
-/* eslint-disable max-len */
-/* eslint-disable jsx-a11y/img-redundant-alt */
-/* eslint-disable @next/next/no-img-element */
 import React from "react";
-import DownloadLink from "../DownloadLink";
-import { SectionSubTitle, SectionTitle } from "../Typography";
+import DownloadLink from "@/components/sections/Download/DownloadLink";
+import { SectionSubTitle, SectionTitle } from "@/components/Typography";
+import { SectionContainer } from "@/components/Containers";
 
 function DownloadDemo() {
   return (
-    <>
-      <section
-        className="w-5/6 max-w-7xl mx-auto pt-20 flex flex-col md:flex-row justify-center items-start"
-        id="demo"
-      >
-        <article className="w-full lg:w-3/5 flex flex-col align-center justify-center gap-4">
-          <SectionTitle className="lg:text-left">
-            Coba
-            <span className="text-blue-600"> Aplikasi</span>
-            <br />
-            Wali Murid
-          </SectionTitle>
-          <SectionSubTitle className="lg:w-3/4 lg:text-left lg:m-0">
-            Anda bisa langsung mencoba menggunakan Aplikasi Demo kami yang
-            tersedia di PlayStore! Jika Anda tertarik dengan menggunakan Ziad
-            System, Silahkan hubungi kami.
-          </SectionSubTitle>
-          <div className="w-full flex items-center justify-center lg:justify-start gap-8 my-4">
-            {/* download link */}
-            <DownloadLink platforms="appstore" />
-            <DownloadLink platforms="playstore" />
-          </div>
-        </article>
-        {/* product image on desktop */}
-        <div className="hidden lg:block">
-          <img
-            src="./demo-app-phone-mockup.png"
-            alt="phone-image-apps"
-            height="auto"
-            className="w-[200px] md:w-[300px]"
-          />
+    <SectionContainer
+      className="max-w-screen-lg pt-40 flex flex-col lg:flex-row"
+      id="demo"
+    >
+      <article className="w-full lg:w-3/5 flex flex-col align-center justify-center gap-4">
+        <SectionTitle className="lg:text-left lg:px-0">
+          Coba
+          <span className="text-blue-600"> Aplikasi</span>
+          <br />
+          Wali Murid
+        </SectionTitle>
+        <SectionSubTitle className="text-center lg:w-3/4 lg:text-left lg:m-0">
+          Anda bisa langsung mencoba menggunakan Aplikasi Demo kami yang
+          tersedia di PlayStore! Jika Anda tertarik dengan menggunakan Ziad
+          System, Silahkan hubungi kami.
+        </SectionSubTitle>
+        <div className="w-full flex items-center justify-center lg:justify-start gap-8 my-4">
+          <DownloadLink platforms="playstore" />
         </div>
-      </section>
-    </>
+      </article>
+
+      <div className="hidden lg:flex grow justify-center">
+        <img
+          src="./demo-app-phone-mockup.png"
+          alt="phone-image-apps"
+          className="w-[200px] md:w-[300px]"
+        />
+      </div>
+    </SectionContainer>
   );
 }
 
