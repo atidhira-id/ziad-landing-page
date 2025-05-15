268f4a0557e19b1e50a9db7ce8076247b08299b0
Atidhira Habibillah <atidhiraid68@gmail.com>
2025-05-15 23:13:48 +0700

    feat: add animation

--- src/components/sections/Home/index.jsx
+++ src/components/sections/Home/index.jsx
@@ -1,3 +1,5 @@
+"use client";
+import { motion } from "framer-motion";
 import PhoneMockup from "@/components/sections/Home/PhoneMockup";
 import { IoLogoYoutube } from "react-icons/io";
 import { MdDownload } from "react-icons/md";
@@ -9,49 +11,101 @@ function Home() {
     <>
       <section className="w-full pt-40 flex flex-col" id="home">
         <section className="w-full flex flex-col items-center justify-center text-center">
-          <h1 className="text-3xl leading-snug sm:text-5xl md:text-6xl lg:text-7xl font-bold tracking-wide text-gray-900 px-4">
-            <span className="text-brand font-extrabold leading-relaxed">
-              ZIAD
-            </span>
-            <br />
-            Sahabat Digitalisasi Pesantren
-          </h1>
-          <SectionSubTitle className="px-4 my-8">
-            Dukung kemajuan lembaga Anda dengan platform all-in-one: mulai dari
-            manajemen siswa, pembayaran digital, e-learning, hingga komunikasi
-            wali murid — semuanya dalam satu tempat.
-          </SectionSubTitle>
-          <div className="w-3/4 flex flex-col sm:flex-row sm:justify-center sm:my-12 gap-4 lg:gap-6">
-            <a
-              href="./brosur-ziad.pdf"
-              download
-              className="w-full max-w-xs mx-auto sm:mx-0"
-            >
-              <Button
-                theme="blue"
-                className="w-full max-w-xs px-2 py-3 justify-center items-center gap-2"
+          {/* ANIMATED HEADING TEXT */}
+          <div className="font-bold tracking-wide text-gray-900 px-4">
+            <div className="mb-8 overflow-hidden">
+              <motion.span
+                key={"ZIAD"}
+                initial={{ translateY: "100%" }}
+                animate={{ translateY: "0" }}
+                transition={{
+                  delay: 0.1,
+                  duration: 0.5,
+                  ease: "easeOut",
+                }}
+                className="text-brand inline-block font-extrabold text-5xl md:text-6xl lg:text-7xl"
               >
-                <MdDownload size={20} />
-                Download Brosur
-              </Button>
-            </a>
-            <a
-              href="https://www.youtube.com/@ptmitracerdasnusantara"
-              target="_blank"
-              className="w-full max-w-xs mx-auto sm:mx-0"
-            >
-              <Button
-                theme="white"
-                className="w-full max-w-xs px-2 py-3 justify-center items-center gap-2"
-              >
-                <IoLogoYoutube size={20} />
-                Lihat Demo
-              </Button>
-            </a>
+                ZIAD
+              </motion.span>
+            </div>
+            <div className="flex flex-wrap justify-center items-center">
+              {["Sahabat", "Digitalisasi", "Pesantren"].map((text, index) => {
+                return (
+                  <div className="pb-2 overflow-hidden" key={text}>
+                    <motion.span
+                      initial={{ translateY: "120%" }}
+                      animate={{ translateY: "0" }}
+                      transition={{
+                        delay: (index + 1) * 0.3,
+                        duration: 0.5,
+                        ease: "easeOut",
+                      }}
+                      className="px-2 inline-block text-3xl md:text-6xl lg:text-7xl"
+                    >
+                      {text}
+                    </motion.span>
+                  </div>
+                );
+              })}
+            </div>
           </div>
+
+          <motion.div
+            initial={{ opacity: 0, y: 30 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{
+              delay: 0.3 * 5,
+              duration: 0.5,
+              ease: "easeOut",
+            }}
+            className="w-full"
+          >
+            <SectionSubTitle className="px-4 my-8">
+              Dukung kemajuan lembaga Anda dengan platform all-in-one: mulai
+              dari manajemen siswa, pembayaran digital, e-learning, hingga
+              komunikasi wali murid — semuanya dalam satu tempat.
+            </SectionSubTitle>
+            <div className="w-3/4 mx-auto flex flex-col sm:flex-row sm:justify-center sm:my-12 gap-4 lg:gap-6">
+              <a
+                href="./brosur-ziad.pdf"
+                download
+                className="w-full max-w-xs mx-auto sm:mx-0 z-0"
+              >
+                <Button
+                  theme="blue"
+                  className="w-full max-w-xs px-2 py-3 justify-center items-center gap-2"
+                >
+                  <MdDownload size={20} />
+                  Download Brosur
+                </Button>
+              </a>
+              <a
+                href="https://www.youtube.com/@ptmitracerdasnusantara"
+                target="_blank"
+                className="w-full max-w-xs mx-auto sm:mx-0 z-0"
+              >
+                <Button
+                  theme="white"
+                  className="w-full max-w-xs px-2 py-3 justify-center items-center gap-2"
+                >
+                  <IoLogoYoutube size={20} />
+                  Lihat Demo
+                </Button>
+              </a>
+            </div>
+          </motion.div>
         </section>
 
-        <section className="mt-8 md:mt-12">
+        <motion.section
+          initial={{ opacity: 0, y: 30 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.3 * 5,
+            duration: 0.5,
+            ease: "easeOut",
+          }}
+          className="mt-8 md:mt-12"
+        >
           <div className="w-full max-w-md md:max-w-lg lg:max-w-3xl m-auto relative flex justify-center items-center">
             <PhoneMockup
               className="w-40 md:w-48 lg:w-72 absolute top-6 left-0"
@@ -66,7 +120,7 @@ function Home() {
               imageUrl="./jumbotron-phone-mockup2.png"
             />
           </div>
-        </section>
+        </motion.section>
       </section>
     </>
   );
