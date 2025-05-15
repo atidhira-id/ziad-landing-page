422d4d3d35945f5fbdd78820fb9cfee5bbabddd2
Atidhira Habibillah <atidhiraid68@gmail.com>
2025-05-15 23:15:22 +0700

    refactor: use looping to add cards

--- src/components/sections/Pricing/index.jsx
+++ src/components/sections/Pricing/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import CardPricing from "@/components/sections/Pricing/CardPricing";
 import { SectionTitle } from "@/components/Typography";
 import { SectionContainer } from "@/components/Containers";
+import { pricing } from "@/data/data";
 
 function Pricing() {
   return (
@@ -10,56 +11,18 @@ function Pricing() {
         Mulai Gunakan Sistem Kami
       </SectionTitle>
       <section className="w-full flex flex-col md:flex-row items-center justify-center gap-10 mb-10 flex-wrap">
-        <CardPricing
-          name="Bulanan"
-          shortDesc="Khusus untuk anak Yatim dan Duafa"
-          price="0"
-          type="primary"
-          desc={[
-            "Dapat semua fitur",
-            "Bebas biaya registrasi",
-            "Bebas biaya pemasangan",
-            "Gratis penyesuaian kebutuhan",
-            "Gratis fitur Baru",
-            "Harga dapat didiskusikan",
-          ]}
-        />
-        <CardPricing
-          name="Bulanan"
-          shortDesc="Per Siswa Aktif"
-          price="15"
-          desc={[
-            "Dapat semua fitur",
-            "Bebas biaya registrasi",
-            "Bebas biaya pemasangan",
-            "Gratis penyesuaian kebutuhan",
-            "Gratis fitur Baru",
-            "Harga dapat didiskusikan",
-            "Harga khusus hubungi team Marketing",
-          ]}
-        />
-        <CardPricing
-          name="Smart Reader"
-          price="400"
-          shortDesc="Per Unit"
-          desc={[
-            "Bayar Sekali",
-            "Garansi Selamanya",
-            "Satu alat bisa banyak fungsi",
-            "Tidak ada minimal pembelian",
-          ]}
-        />
-        <CardPricing
-          name="Smart Card"
-          shortDesc="Per Unit"
-          price="15"
-          desc={[
-            "Bayar Sekali",
-            "Full print dua sisi",
-            "Design bebas",
-            "Kartu multifungsi",
-          ]}
-        />
+        {pricing.map((item, key) => {
+          return (
+            <CardPricing
+              key={key}
+              name={item.name}
+              type={item.type || ""}
+              price={item.price}
+              shortDesc={item.shordDesc}
+              desc={item.desc}
+            />
+          );
+        })}
       </section>
     </SectionContainer>
   );
